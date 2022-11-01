f043f465e748d39b96495c3fe791c3c39714b16e
Imesh <2019cs171@stu.ucsc.lk>
2022-11-01 22:43:30 +0530

    second update

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: mobileBackgroundColor,
       ),
-      home: Scaffold(body: Text('Lets build'),)
+      home: Scaffold(body: Text('Lets build the app'),)
     );
   }
 }
