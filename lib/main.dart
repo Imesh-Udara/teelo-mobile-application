7d5567aa62b3cdabb5f9cab2439610a2caffe096
Imesh <2019cs171@stu.ucsc.lk>
2022-11-02 14:00:14 +0530

    Sign In UI
    Complete signIn UI and setting up basic firebase setting

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:teelo_flutter/responsive/mobile_screen_Layout.dart';
+import 'package:teelo_flutter/responsive/responsive_layout_screen.dart';
+import 'package:teelo_flutter/responsive/web_screen_Layout.dart';
+import 'package:teelo_flutter/screens/login_screen.dart';
 import 'package:teelo_flutter/utils/colors.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: 'AIzaSyA1VUcb2fsmVJmHuPIP7bqgzVfjaK914g0',
+            appId: '1:148999610966:web:60160e5d8b068ac44d91f7',
+            messagingSenderId: '148999610966',
+            projectId: 'teelo-mobile-app',
+            storageBucket: 'teelo-mobile-app.appspot.com'));
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
@@ -12,13 +31,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Teelo social app',
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: mobileBackgroundColor,
-      ),
-      home: Scaffold(body: Text('Lets build the app'),)
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Teelo social app',
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: mobileBackgroundColor,
+        ),
+        // home: ResponsiveLayout(
+        //   mobileScreenLayout: MobileScreenLayout(),
+        //   webScreenLayout: WebScreenLayout(),
+        // )
+        home: LoginScreen(),
+        );
   }
 }
-
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:teelo_flutter/utils/colors.dart';
+import 'package:teelo_flutter/widgets/text_field_input.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+            child: Container(
+      padding: EdgeInsets.symmetric(horizontal: 32),
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Flexible(
+            child: Container(),
+            flex: 2,
+          ),
+          //svg imag
+          SvgPicture.asset(
+            'assets/Teelo.svg',
+            color: primaryColor,
+            height: 64,
+          ),
+          const SizedBox(
+            height: 64,
+          ),
+          TextFieldInput(
+              textEditingController: _emailController,
+              hintText: 'Enter your Email',
+              textInputType: TextInputType.emailAddress),
+          const SizedBox(
+            height: 24,
+          ),
+          TextFieldInput(
+            textEditingController: _passwordController,
+            hintText: 'Enter your Password',
+            textInputType: TextInputType.text,
+            isPass: true,
+          ),
+          const SizedBox(
+            height: 24,
+          ),
+          InkWell(
+            child: Container(
+              child: const Text('Log in'),
+              padding: const EdgeInsets.symmetric(vertical: 16),
+              width: double.infinity,
+              alignment: Alignment.center,
+              decoration: const ShapeDecoration(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(4))),
+                color: blueColor,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          Flexible(
+            child: Container(),
+            flex: 2,
+          ),
+          //This another row foe signup line
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                child: Text("Dont't have an account? "),
+                padding: const EdgeInsets.symmetric(vertical: 8),
+              ),
+              GestureDetector(
+                onTap: () {
+                  
+                },
+                child: Container(
+                  child: Text(
+                    "Sign Up",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+        ],
+      ),
+    )));
+  }
+}
